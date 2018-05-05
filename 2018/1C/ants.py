0c3fc8515653ef63cc9b4bce5659c9a9a410250f
amauryliet <amaury.liet@hotmail.fr>
2018-05-05 21:28:29 +0200

    Discover infinity

--- 2018/1C/ants.py
+++ 2018/1C/ants.py
@@ -1,3 +1,5 @@
+from math import inf
+
 N = int(input())
 
 for case_id in range(1, N + 1):
@@ -9,12 +11,9 @@ for case_id in range(1, N + 1):
     for weight in weights:
         for left_pile_length, left_pile_accumulated_weight in [*min_weight_for_length.items()]:
             if left_pile_accumulated_weight <= 6*weight:
-                previous_accumulated_weight = min_weight_for_length.get(left_pile_length + 1, -1)
-                tested_accumulated_weight = left_pile_accumulated_weight + weight
-                
                 min_weight_for_length[left_pile_length + 1] = min(
-                    previous_accumulated_weight,
-                    tested_accumulated_weight
-                ) if previous_accumulated_weight != -1 else tested_accumulated_weight
+                    min_weight_for_length.get(left_pile_length + 1, inf),
+                    left_pile_accumulated_weight + weight,
+                )
     
     print('Case #{}: {}'.format(case_id, max(min_weight_for_length)))
