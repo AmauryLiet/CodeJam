837f36d092762ba788d6162023ec4a6ab8b26af5
amauryliet <amaury.liet@hotmail.fr>
2021-03-27 08:24:45 +0100

    2021 qualif 3

--- 2021/0_qualif/1.py
+++ 2021/0_qualif/1.py
@@ -1,13 +1,15 @@
-# import sys
-# DEBUG = True
-# log = lambda *a, **k: print(*a, **k, file=sys.stderr) if DEBUG else None
+import sys
+
+DEBUG = False
+log = lambda *a, **k: print(*a, **k, file=sys.stderr) if DEBUG else None
 
 N = int(input())
 
 
-for case_id in range(1, N + 1):
-    length = int(input())
-    array_to_sort = [*map(int, input().split())]
+def array_cost(array_to_sort):
+    length = len(array_to_sort)
+
+    log(length, array_to_sort)
 
     total_cost = 0
     for index in range(0, length - 1):
@@ -15,9 +17,16 @@ for case_id in range(1, N + 1):
         min_value_index = array_to_sort.index(min_value)
         total_cost += min_value_index + 1
         array_to_sort = array_to_sort[0:min_value_index][::-1] + array_to_sort[min_value_index + 1:]
+        log('+', min_value_index + 1, ':', array_to_sort)
+    return total_cost
+
+
+for case_id in range(1, N + 1):
+    int(input())
+    array_to_sort = [*map(int, input().split())]
+    total_cost = array_cost(array_to_sort)
 
     print('Case #{}: {}'.format(case_id, total_cost))
-    # log(length, array_to_sort)
 
 # 1 5 2 3 4
 # +1
@@ -34,3 +43,5 @@ for case_id in range(1, N + 1):
 # 1 3 4 5 2 +4
 # 1 2 5 4 3 +
 # 1 2 3 4 5 *2
+
+# 2 4 6 7 5 3 1
--- 2021/0_qualif/3.py
+++ 2021/0_qualif/3.py
@@ -1,5 +1,70 @@
+import sys
+
+DEBUG = False
+log = lambda *a, **k: print(*a, **k, file=sys.stderr) if DEBUG else None
+
 N = int(input())
 
+
+def array_cost(array_to_sort):
+    # log('[computing cost]', array_to_sort)
+
+    length = len(array_to_sort)
+
+    total_cost = 0
+    for index in range(0, length - 1):
+        min_value = index + 1
+        min_value_index = array_to_sort.index(min_value)
+        total_cost += min_value_index + 1
+        array_to_sort = array_to_sort[0:min_value_index][::-1] + array_to_sort[min_value_index + 1:]
+        # log('[computing cost] +', min_value_index + 1, ':', array_to_sort)
+    return total_cost
+
+
 for case_id in range(1, N + 1):
-    x, y = map(int, input().split())
-    print('Case #{}: {}'.format(case_id, 0))
+    target_length, target_cost = map(int, input().split())
+    min_cost = target_length - 1
+    max_cost = (target_length + 2) * (target_length - 1) // 2
+
+    if not (min_cost <= target_cost <= max_cost):
+        print('Case #{}: IMPOSSIBLE'.format(case_id))
+        continue
+
+    array_prefix = []
+    array_suffix = []
+
+    cumulated_cost = 0
+    is_array_inverted = False
+    for number_to_place in range(1, target_length):
+        min_cost_of_following_numbers = target_length - number_to_place - 1
+        max_cost_induced_by_number = target_length - number_to_place + 1
+
+        use_max_cost = cumulated_cost + max_cost_induced_by_number + min_cost_of_following_numbers <= target_cost
+        # log('[building] placing {} ({}<=cost<={}+{}) with {} to add: {}({})'.format(
+        #     number_to_place,
+        #     min_cost_of_following_numbers + 1,
+        #     min_cost_of_following_numbers,
+        #     max_cost_induced_by_number, target_cost - cumulated_cost,
+        #     'after' if use_max_cost else 'before',
+        #     max_cost_induced_by_number if use_max_cost else 1,
+        # ))
+
+        if use_max_cost == is_array_inverted:
+            array_prefix.append(number_to_place)
+        else:
+            array_suffix.insert(0, number_to_place)
+        cumulated_cost += max_cost_induced_by_number if use_max_cost else 1
+        if use_max_cost:
+            is_array_inverted = not is_array_inverted
+
+    unsorted_array = array_prefix + [target_length] + array_suffix
+
+    log('[case] goal:', target_cost, target_length,
+        'answer:', array_cost(unsorted_array), len(unsorted_array), unsorted_array)
+    print('Case #{}: {}'.format(case_id, ' '.join(map(str, unsorted_array))))
+
+# length 3:
+# 2: 1 2 3 (1+1)
+# 3: (1+2) [autre : (2+1)]
+# 4: (3+1) ou
+# 5: 2 3 1 (3+2)
