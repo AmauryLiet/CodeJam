fcede89de62ce1d5b6abd9274e70149f04a0e105
amauryliet <amaury.liet@hotmail.fr>
2021-05-15 16:19:21 +0200

    2/1

--- 2021/2/1.py
+++ 2021/2/1.py
@@ -3,8 +3,16 @@ import sys
 DEBUG = True
 log = lambda *a, **k: print(*a, **k, file=sys.stderr) if DEBUG else None
 
-N = int(input())
+N, array_length = map(int, input().split())
 
 for case_id in range(1, N + 1):
-    x, y = map(int, input().split())
-    print('Case #{}: {}'.format(case_id, 0))
+    sorted_until_pos_incl = 0
+    while sorted_until_pos_incl <= 98:
+        print('M {} {}'.format(sorted_until_pos_incl + 1, array_length))
+        pos_min = int(input())
+        if pos_min != sorted_until_pos_incl + 1:
+            print('S {} {}'.format(sorted_until_pos_incl + 1, pos_min))
+            input()  # 1
+        sorted_until_pos_incl += 1
+    print('D')  # we are done
+    input()
